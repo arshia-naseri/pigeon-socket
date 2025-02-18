2d01040c1179df534d706452e29a726785f93d0f
Arshia Naseri <arshia.naseri.w@outlook.com>
2025-02-18 00:54:13 -0500

    + Admin UI

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-// const {instrument} = require("@socket.io/admin-ui")
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { instrument } = require("@socket.io/admin-ui");
 
-const updateChatMessageURL = process.env.REACT_APP_GET_UPDATE_MESSAGES_URL || "http://localhost:5040/updateChatMessage";
-const io = require('socket.io')(5050, {
-    cors: {
-        origin: '*',
-        methods: ["GET", "POST"]
-    },
-    transports: ['websocket','polling'], 
-    upgrade: false
-},);
+const httpServer = createServer();
 
-io.on('connection', socket => {
-    // console.log(socket.id);
+const io = new Server(httpServer, {
+  cors: {
+    origin: [
+      "https://admin.socket.io",
+      "http://localhost:3000",
+      "https://arshia-naseri.github.io/pigeon_chat_client_2.0/",
+    ],
+    credentials: true,
+  },
+});
 
-    socket.on("send_message", (from, time, text, roomID) => {
-        let id = from.id;
-        delete from.id;
-        socket.to(roomID).emit("receive_message", {from, time, text, roomID})
-        
-        fetch(updateChatMessageURL, {
-            method: 'POST',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify({id, time, text, roomID})
-        })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err))
-        
-        
-    })
+io.on("connection", (socket) => {
+  console.log("Connected id:", socket.id);
 
-    socket.on("join-room", (roomList) => {
-        // console.log(roomList);
-        socket.join(roomList);
-    })
-})
+  socket.on("send_message", (from, time, text, roomID) => {
+    let id = from.id;
+    delete from.id;
+    socket.to(roomID).emit("receive_message", { from, time, text, roomID });
+  });
 
-// instrument(io, {auth:false,mode: "development"})
+  socket.on("join-room", (roomList) => {
+    // console.log(roomList);
+    socket.join(roomList);
+  });
+});
+
+instrument(io, { auth: false, mode: "development" });
+
+httpServer.listen(5050);
